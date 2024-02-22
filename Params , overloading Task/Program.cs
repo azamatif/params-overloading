3e0e80e75af8170116a26ca6cd65c17215a4aaa4
azamatif <azamatif@code.edu.az>
2024-02-22 15:06:37 +0100

    #adding  home task

--- Params , overloading Task/Program.cs
+++ Params , overloading Task/Program.cs
@@ -1,3 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+#region Task2
+//Console.WriteLine(WhichSeason(5));
+
+//static string WhichSeason(params int[] months)
+//{
+//    int mont = months[0];
+//    switch (months) { }
+//case3:
+//case4:
+//case5:
+//    return "yaz";
+//case6:
+//case7:
+//case8:
+//    return "yay";
+//case9:
+//case10:
+//case11:
+//    return "payiz";
+//case12:
+//case1:
+//case2:
+//    return "gis";
+
+//    return "hec biri";
+//}
+#endregion
+#region Task 3
+
+//    Console.WriteLine(Multiplied(2, 3, 4));
+
+//static int Multiplied(params int[] values)
+//{
+//    int result = 1;
+
+//    foreach (var item in values)
+//    {
+//        result *= item;
+//    }
+
+//    return result;
+//}
+
+#endregion
+#region Task1
+//Console.WriteLine(ThreeValue(5, 50, 120));
+
+// static int ThreeValue(params int[] numbers)
+//{
+//    int result = 0;
+
+//    for (int i = 0; i < numbers.Length; i++)
+//    {
+//        if (numbers[i] > 10 && numbers[i]<=100)
+//        {
+//            result += numbers[i];
+//        }
+//    }return result;
+//}
+
+
+
+#endregion 
 
